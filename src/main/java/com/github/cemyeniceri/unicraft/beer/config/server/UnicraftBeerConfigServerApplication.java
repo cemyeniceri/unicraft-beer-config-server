baf5fa9d52d6d4203301b920481cb4f2ac445be9
cyeniceri <cem.yeniceri@gmail.com>
2020-12-05 13:18:12 +0000

    Initialize Config Server

---
+++ src/main/java/com/github/cemyeniceri/unicraft/beer/config/server/UnicraftBeerConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.cemyeniceri.unicraft.beer.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicraftBeerConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnicraftBeerConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/cemyeniceri/unicraft/beer/config/server/UnicraftBeerConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cemyeniceri.unicraft.beer.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnicraftBeerConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
