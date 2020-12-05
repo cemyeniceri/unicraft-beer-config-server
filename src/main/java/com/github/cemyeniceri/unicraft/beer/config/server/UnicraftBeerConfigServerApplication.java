8f4803ef3d2900abe32aab01ce614c49560a0768
cyeniceri <cem.yeniceri@gmail.com>
2020-12-05 13:37:10 +0000

    config repo url is set up

--- src/main/java/com/github/cemyeniceri/unicraft/beer/config/server/UnicraftBeerConfigServerApplication.java
+++ src/main/java/com/github/cemyeniceri/unicraft/beer/config/server/UnicraftBeerConfigServerApplication.java
@@ -2,7 +2,9 @@ package com.github.cemyeniceri.unicraft.beer.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class UnicraftBeerConfigServerApplication {
 
